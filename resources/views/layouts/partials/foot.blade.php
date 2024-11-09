b053b37feb3b3d17ccfcf09344370623fb2d04c5
dhaydt <1992dayat@gmail.com>
2024-11-09 16:32:30 +0700

    script

--- resources/views/layouts/partials/foot.blade.php
+++ resources/views/layouts/partials/foot.blade.php
@@ -1,2 +1,3 @@
 <script src="{{ asset('assets/bootstrap-5.3.3-dist/js/bootstrap.bundle.min.js') }}"></script>
-<script src="{{ asset('assets/js/script.js') }}"></script>
+{{-- <script src="{{ asset('assets/js/script.js') }}"></script> --}}
+<script src="{{ asset('script.js') }}"></script>
